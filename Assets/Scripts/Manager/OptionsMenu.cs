c323103cfa169b2a1bf774ae90efc206f8cf35c1
Martí Marín <mmarinmunoz26@gmail.com>
2023-05-09 16:14:48 +0200

    New Update menu

---
+++ Assets/Scripts/Manager/OptionsMenu.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class OptionsMenu : MonoBehaviour
+{
+    public Slider slider;
+    public float sliderValue;
+    public Image imageMute;
+
+    void Start()
+    {
+        slider.value = State.volume;
+        AudioListener.volume = slider.value;
+        RevisarSiEstoyMute();
+    }
+
+    public void ChangeSlider(float valor)
+    {
+        State.volume = valor;
+        sliderValue = State.volume;
+        AudioListener.volume = slider.value;
+        RevisarSiEstoyMute();
+    }
+    public void RevisarSiEstoyMute()
+    {
+        if (sliderValue == 0)
+        {
+            imageMute.enabled = true;
+        }
+        else
+        {
+            imageMute.enabled = false;
+        }
+    }
+}
+
+public class State
+{
+    public static float volume = 0.5f;
+
+}
