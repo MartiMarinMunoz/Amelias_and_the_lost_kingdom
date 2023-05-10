740b2ff25f66884f23daec301631ef9fa86f8eca
Martí Marín <mmarinmunoz26@gmail.com>
2023-05-10 14:24:27 +0200

    New update Audio and Scroll Text (History and Lore)

--- Assets/Scripts/Manager/HealthCollectable.cs
+++ Assets/Scripts/Manager/HealthCollectable.cs
@@ -1,16 +1,20 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Audio;
 
 public class HealthCollectable : MonoBehaviour
 {
+    public AudioSource clip;
     
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if(collision.tag == "Player")
         {
             collision.GetComponent<HealthController>().UpdateHoja();
-            Destroy(gameObject);
+            Destroy(gameObject, 0.1f);
+
+            clip.Play();
         }
     }
 }
